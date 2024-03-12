34f0e973835f1595341bbdebde09f5ed6ba6bab9
Beau Bureau <beau_bureau@hotmail.com>
2024-03-12 08:21:50 -0500

    basics

--- soundcurtain-app/src/app/common/clock/clock.component.ts
+++ soundcurtain-app/src/app/common/clock/clock.component.ts
@@ -11,13 +11,31 @@ export class ClockComponent  {
     minutes:any;
     seconds:any;
     currentLocale: any;
-  
+    timeOfDay: string = '';
+
     isTwelveHrFormat = false;
     test:any;
     constructor(){
       setInterval(() =>{
      const currentDate = new Date();
      this.date = currentDate.toLocaleTimeString();
+     this.calculateTimeOfDay()
       }, 1000);
+    }
+
+    calculateTimeOfDay() {
+      const currentHour = new Date().getHours();
+  
+      if (currentHour >= 6 && currentHour < 11) {
+        this.timeOfDay = 'morning';
+      } else if (currentHour >= 11 && currentHour < 18) {
+        this.timeOfDay = 'afternoon';
+      }
+      else if (currentHour >= 18 && currentHour < 20) {
+        this.timeOfDay = 'sunset';
+      }
+       else {
+        this.timeOfDay = 'night';
+      }
     }
   }
