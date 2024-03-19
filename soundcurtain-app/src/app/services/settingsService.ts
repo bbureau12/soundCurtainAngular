a94bf95417e3cdb5410c7410570927d14d6c3705
Beau Bureau <beau_bureau@hotmail.com>
2024-03-19 18:48:21 -0500

    cleanup

--- soundcurtain-app/src/app/services/settingsService.ts
+++ soundcurtain-app/src/app/services/settingsService.ts
@@ -1,4 +1,4 @@
-import { catchError, map, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
